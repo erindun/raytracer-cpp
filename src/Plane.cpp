a509b3273724b0bdc1ba0340bcfe742c201e00a2
Garrett Duncan <garrettdunc@gmail.com>
2021-02-16 15:56:46 -0500

    Add Plane intersection function

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -8,6 +8,18 @@ Plane::Plane(const Vector &p, const Vector &d, const Color &c)
     : SceneObject{c}, position{p}, norm_direction{d} {}
 
 float Plane::intersection(const Ray &r) const {
-  // TODO
-  return 0.0f;
+  float dn = dot(r.get_direction(), this->norm_direction);
+  if (dn == 0) {
+    // Ray is parallel to plane (no intersection).
+    return -1.0f;
+  } else {
+    float t =
+        dot(-(r.get_position() - this->position), this->norm_direction) / dn;
+
+    if (t < 0)
+      // Not an intersection.
+      return -1.0f;
+    else
+      return t;
+  }
 }
