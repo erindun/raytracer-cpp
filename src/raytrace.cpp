30f1778253c3c35a8608aa2b2780716e7d390008
Garrett Duncan <garrettdunc@gmail.com>
2021-02-17 09:02:07 -0500

    Cleanup

--- src/raytrace.cpp
+++ src/raytrace.cpp
@@ -16,9 +16,9 @@ Color Trace(const Ray &r, const Scene &s) {
   std::map<float, SceneObject *> intersections;
 
   for (auto object : s) {
-    float intersection = object->intersection(r);
-    if (intersection > 0)
-      intersections.insert({intersection, object});
+    float intersect = object->intersection(r);
+    if (intersect > 0)
+      intersections.insert({intersect, object});
   }
 
   return intersections.begin()->second->get_color();
@@ -31,7 +31,7 @@ int main() {
   Sphere sphere{Vector{1.0f, 2.0f, 15.0f}, 3.0f, Color{0.5, 1.0, 0.0}};
 
   Camera camera{Vector{0.0f, 0.0f, 0.0f}, Vector{0.0f, 0.0f, 1.0f},
-                Vector{0.0f, 1.0f, 0.0f}, 90.f, 1.3333f};
+                Vector{0.0f, 1.0f, 0.0f}, 90.0f, 1.3333f};
 
   ImagePlane image{1024, 768};
   Scene scene{&plane, &sphere};
