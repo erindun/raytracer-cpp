8c9f7b1df844ab66757057c8eb40ac6079e24800
Garrett Duncan <garrettdunc@gmail.com>
2021-02-24 00:46:49 -0500

    Remove limits inclusion

--- Trace.cpp
+++ Trace.cpp
@@ -1,12 +1,11 @@
 #include "Trace.h"
 #include "Ray.h"
 #include "SceneObject.h"
-#include <limits>
 #include <map>
 using namespace chromeball;
 
 Color Trace(const Ray &r, const Scene &s) {
-  float tmin = std::numeric_limits<float>::infinity();
+  float tmin = 1e20;
   Color c{0, 0, 0};
   for (auto obj : s) {
     float t = obj->intersection(r);
