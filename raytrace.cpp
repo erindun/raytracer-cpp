78b6395e1da67b2466a59ba078b34214734e0df5
Garrett Duncan <garrettdunc@gmail.com>
2021-02-27 13:37:11 -0500

    Fix scene for 4050 submission

--- raytrace.cpp
+++ raytrace.cpp
@@ -20,17 +20,9 @@ const int NY = 768;
 int to_hex(double color) { return color * 255; }
 
 int main() {
-  Plane plane0{Vector{0.0f, 2.0f, 0.0f}, Vector{0.0f, -1.0f, 0.0f},
-               Color{1, 1, 1}};
-  Plane plane1{Vector{0.0f, -2.0f, 0.0f}, Vector{0.0f, 1.0f, 0.0f},
-               Color{1, 1, 1}};
-  Plane plane2{Vector{-2.0f, 0.0f, 0.0f}, Vector{1.0f, 0.0f, 0.0f},
-               Color{1, 0, 0}};
-  Plane plane3{Vector{2.0f, 0.0f, 0.0f}, Vector{-1.0f, 0.0f, 0.0f},
-               Color{0, 1, 0}};
-  Plane plane4{Vector{0.0f, 0.0f, 10.0f}, Vector{0.0f, 0.0f, -1.0f},
-               Color{1, 1, 1}};
-  Sphere sphere{Vector{1.1f, 1.25f, 7.0f}, 1.0f, Color{0.5, 0.5, 0.5}};
+  Plane plane{Vector{0.0f, 2.0f, 0.0f}, Vector{0.0f, 1.0f, 0.0f},
+              Color{0.0, 0.5, 1.0}};
+  Sphere sphere{Vector{1.0f, 2.0f, 15.0f}, 3.0f, Color{0.5, 1.0f, 0.0f}};
 
   Camera camera{Vector{0.0f, 0.0f, 0.0f}, Vector{0.0f, 0.0f, 1.0f},
                 Vector{0.0f, 1.0f, 0.0f}, 90.0f, 1.3333f};
@@ -38,7 +30,7 @@ int main() {
   PointLight light{Vector{-1.0f, -1.0f, 7.0f}, Color{2, 2, 2}};
 
   ImagePlane image{NX, NY};
-  Scene scene{&plane0, &plane1, &plane2, &plane3, &plane4, &sphere};
+  Scene scene{&plane, &sphere};
 
   std::ofstream ppm("output.ppm");
   ppm << "P3" << std::endl << NX << " " << NY << std::endl;
